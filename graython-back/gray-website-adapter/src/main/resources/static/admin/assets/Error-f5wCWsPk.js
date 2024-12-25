28405c6e0dd1f0e36bdc17a55e1e7954c51ecfad
pengjunlee <pengjunlee@163.com>
2024-12-25 10:47:21 +0800

    删除admin模块，另起项目

--- graython-back/gray-website-adapter/src/main/resources/static/admin/assets/Error-f5wCWsPk.js
+++
@@ -1,1 +0,0 @@
-import{ae as e,k as o,l as t,q as c}from"./main-CTOmdTUB.js";const r={},s=c("p",null,"发生错误了",-1),n=[s];function a(_,l){return o(),t("div",null,n)}const f=e(r,[["render",a]]);export{f as default};
